6d0ba0f856292e68f51645f6fa90f249a1babd5f
mkhusyainov <khusyainov.mar@yandex.ru>
2022-07-15 12:45:11 +0300

    1

--- src/main/java/Cast/Animal.java
+++ src/main/java/Cast/Animal.java
@@ -3,6 +3,7 @@ package Cast;
 public abstract class Animal {
     String name;
     int age;
+    int agile;
     String nameOfClass = getClass().getSimpleName();
     public void eat(){
         System.out.println(nameOfClass);
