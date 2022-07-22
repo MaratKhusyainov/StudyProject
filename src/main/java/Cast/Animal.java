c25a25e47d6f3a17ac8d945cff9ee3020982341b
mkhusyainov <khusyainov.mar@yandex.ru>
2022-07-22 13:00:55 +0300

    Test

--- src/main/java/Cast/Animal.java
+++ src/main/java/Cast/Animal.java
@@ -1,9 +1,8 @@
 package Cast;
 
 public abstract class Animal {
-    String name = "Marat";
-    String surname = "Khusyainov";
-    int age = 33;
+    String name;
+    int age;
     String nameOfClass = getClass().getSimpleName();
     public void eat(){
         System.out.println(nameOfClass);
