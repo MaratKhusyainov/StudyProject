cd512cea4705fd9c6ae295d69f055b23197e29bd
mkhusyainov <khusyainov.mar@yandex.ru>
2022-07-22 12:54:58 +0300

    third commut

--- src/main/java/Cast/Animal.java
+++ src/main/java/Cast/Animal.java
@@ -2,6 +2,7 @@ package Cast;
 
 public abstract class Animal {
     String name = "Marat";
+    String surname = "Khusyainov";
     int age = 33;
     String nameOfClass = getClass().getSimpleName();
     public void eat(){
