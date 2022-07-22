5235727fe84aa76b7ee1ad44b7092d9c4a31b145
mkhusyainov <khusyainov.mar@yandex.ru>
2022-07-22 12:54:33 +0300

    second commut

--- src/main/java/Cast/Animal.java
+++ src/main/java/Cast/Animal.java
@@ -2,7 +2,7 @@ package Cast;
 
 public abstract class Animal {
     String name = "Marat";
-    int age;
+    int age = 33;
     String nameOfClass = getClass().getSimpleName();
     public void eat(){
         System.out.println(nameOfClass);
