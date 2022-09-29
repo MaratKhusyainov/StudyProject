96fbcb43442fdfe8704e973b3fd0a006a4b4cdf3
mkhusyainov <khusyainov.mar@yandex.ru>
2022-09-29 13:19:05 +0300

    Add new parameters

--- src/main/java/Cast/Animal.java
+++ src/main/java/Cast/Animal.java
@@ -4,4 +4,7 @@ public abstract class Animal {
     String type = "Lion";
     int weight = 80;
 
+    String type2 = "Tiger";
+    int weight2 = 100;
+
 }
--- src/main/java/Cast/Human.java
+++ src/main/java/Cast/Human.java
@@ -2,7 +2,9 @@ package Cast;
 
 public class Human {
     public static void main(String[] args) {
-//        String name = "Ivan";
-//        String surname = "Ivanov";
+        int age = 30;
+        String name = "Ivan";
+        String surname = "Ivanov";
+        System.out.println(age);
     }
 }
